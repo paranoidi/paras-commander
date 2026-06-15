6ea81973296ef2ff6647e826de45a46a4ab0a1d4
paranoidi <marko.koivusalo@gmail.com>
2026-06-15 18:53:57 +0300

    feat: Shortcuts + queued symbol change

--- internal/keymap/keymap_test.go
+++ internal/keymap/keymap_test.go
@@ -173,6 +173,8 @@ func TestDefaultLookupMatchesSimulationKeys(t *testing.T) {
 		{tcell.NewEventKey(tcell.KeyRune, 'o', tcell.ModMeta), ActionPanelOpenDirInOther, true},
 		{tcell.NewEventKey(tcell.KeyRune, 'i', tcell.ModMeta), ActionPanelOpenActivePathInOther, true},
 		{tcell.NewEventKey(tcell.KeyRune, 'o', tcell.ModMeta|tcell.ModAlt), ActionPanelOpenDirInOther, true},
+		{tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModShift), ActionPanelOpenDirInOther, true},
+		{tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModShift), ActionPanelOpenActivePathInOther, true},
 		{tcell.NewEventKey(tcell.KeyRune, ',', tcell.ModAlt|tcell.ModShift), ActionPanelMetaEdit, true},
 		{tcell.NewEventKey(tcell.KeyRune, ';', tcell.ModAlt), ActionPanelMetaEdit, true},
 		{tcell.NewEventKey(tcell.KeyRune, ',', tcell.ModAlt), ActionPanelMeta, true},
